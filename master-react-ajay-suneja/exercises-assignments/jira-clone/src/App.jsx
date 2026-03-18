c514aa7b4a9656f67a7dc236ef45cea499adb103
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2026-03-18 17:06:18 +0530

    🚀

--- master-react-ajay-suneja/exercises-assignments/jira-clone/src/App.jsx
+++ master-react-ajay-suneja/exercises-assignments/jira-clone/src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css';
+import TaskColumn from './components/TaskColumn/TaskColumn';
 import TaskForm from './components/TaskForm/TaskForm';
 
 function App() {
   return (
     <div className='app'>
-      <header className='app_header'>Jira Board</header>
+      <header className='app_header'>
+        <h1>Jira Board</h1>
+      </header>
       <TaskForm />
       <main className='app_main'>
-        <section className='task_column'>One</section>
-        <section className='task_column'>Two</section>
-        <section className='task_column'>Three</section>
-        <section className='task_column'>Four</section>
+        <TaskColumn title="Ready for Development" />
+        <TaskColumn title="In Progress" />
+        <TaskColumn title="Ready for Test" />
+        <TaskColumn title="Closed" />
       </main>
     </div>
   );
--- master-react-ajay-suneja/exercises-assignments/jira-clone/src/components/TaskColumn/TaskColumn.jsx
+++ master-react-ajay-suneja/exercises-assignments/jira-clone/src/components/TaskColumn/TaskColumn.jsx
@@ -1,7 +1,31 @@
-function TaskColumn() {
+import './TaskColumn.css';
+
+function TaskColumn(props) {
+  let iconName = null;
+  if (props.title === 'Ready for Development') {
+    iconName = 'code';
+  }
+
+  if (props.title === 'In Progress') {
+    iconName = 'clock_loader_60';
+  }
+
+  if (props.title === 'Ready for Test') {
+    iconName = 'science';
+  }
+
+  if (props.title === 'Closed') {
+    iconName = 'check_box';
+  }
+
   return (
     <div>
-      <section className='task_column'>One</section>
+      <section className='task_column'>
+        <span className="material-symbols-outlined">
+          { iconName }
+        </span>
+        <h2 className='task_column_heading'>{ props.title }</h2>
+      </section>
     </div>
   );
 }
